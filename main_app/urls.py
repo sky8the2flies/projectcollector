7ffeef9ebf5ba9d38cbefb46e21f388072c927a6
skylerseifert <skylerseifert@gmail.com>
2020-09-16 18:09:16 -0700

    Add search

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('projects/', views.projects_index, name='index'),
+    path('projects/', views.SearchResultsView.as_view(), name='search_results'),
     path('projects/create', views.ProjectCreate.as_view(), name='project_create'),
     path('projects/<int:project_id>', views.projects_detail, name='detail'),
     path('projects/<int:pk>/delete', views.ProjectDelete.as_view(), name='project_delete'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.db.models import Q
 
 from .models import Project
 from .forms import TimelineForm
@@ -10,10 +12,6 @@ def home(request):
 
 def about(request):
     return render(request, 'main_app/about.html')
-
-def projects_index(request):
-    projects = Project.objects.all()
-    return render(request, 'main_app/projects/index.html', {'projects': projects})
 
 def projects_detail(request, project_id):
     project = Project.objects.get(id=project_id)
@@ -29,6 +27,20 @@ def add_timeline(request, project_id):
         new_timeline.project_id = project_id
         new_timeline.save()
     return redirect('detail', project_id=project_id)
+
+class SearchResultsView(ListView):
+    model = Project
+    template_name = 'main_app/projects/index.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if not query:
+            return Project.objects.all()
+        return Project.objects.filter(
+            Q(name__icontains=query) | 
+            Q(technologies__icontains=query) |
+            Q(description__icontains=query)
+        )
 
 class ProjectCreate(CreateView):
     model = Project
