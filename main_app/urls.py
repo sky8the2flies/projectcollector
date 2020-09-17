1ce1aa9fb785ffffce6320912263e72b0bd18470
skylerseifert <skylerseifert@gmail.com>
2020-09-16 17:02:36 -0700

    Add edit and create project

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('projects/', views.projects_index, name='index'),
+    path('projects/create', views.ProjectCreate.as_view(), name='project_create'),
     path('projects/<int:project_id>', views.projects_detail, name='detail'),
+    path('projects/<int:pk>/delete', views.ProjectDelete.as_view(), name='project_delete'),
+    path('projects/<int:pk>/update', views.ProjectUpdate.as_view(), name='project_update'),
+    path('projects/<int:project_id>/add_timeline', views.add_timeline, name='add_timeline'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Project
+from .forms import TimelineForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'main_app/home.html')
 
 def about(request):
     return render(request, 'main_app/about.html')
@@ -16,4 +17,27 @@ def projects_index(request):
 
 def projects_detail(request, project_id):
     project = Project.objects.get(id=project_id)
-    return render(request, 'main_app/projects/detail.html', {'project': project})
+    timeline_form = TimelineForm()
+    return render(request, 'main_app/projects/detail.html', {
+        'project': project, 'timeline_form': timeline_form
+    })
+
+def add_timeline(request, project_id):
+    form = TimelineForm(request.POST)
+    if form.is_valid():
+        new_timeline = form.save(commit=False)
+        new_timeline.project_id = project_id
+        new_timeline.save()
+    return redirect('detail', project_id=project_id)
+
+class ProjectCreate(CreateView):
+    model = Project
+    fields = '__all__'
+
+class ProjectUpdate(UpdateView):
+    model = Project
+    fields = '__all__'
+
+class ProjectDelete(DeleteView):
+    model = Project
+    success_url = '/projects/'
